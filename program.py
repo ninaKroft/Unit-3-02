defad091931f76029f9f4d461e71b9f268c9d5ed
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-04-12 18:50:23 +0000

    yay

--- program.py
+++ program.py
@@ -1,1 +1,15 @@
+#Create a program that calculates the area of a trapezoid based on user input
+
+def area():
+    print("What is the length of a?")
+    a = float(input())
+    print("What is the length of b?")
+    b = float(input())
+    print("What is the length of h?")
+    h = float(input())
+    calcArea = h * (a + b) / 2
+    return calcArea
+
+print("The trapezoid has an area of:", area())
+
 
